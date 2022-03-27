3a794763869a0f6cd389c8ab9839e592f50ff199
Laurenz Ruzicka <laurenz_ruzicka@gmx.at>
2022-03-27 22:15:25 +0200

    Added callbacks to command_manager Added selection window changed data input to functions input to automatically refresh data on callback

--- src/SoundEditor/DataView.py
+++ src/SoundEditor/DataView.py
@@ -1,5 +1,7 @@
+from __future__ import annotations
+
 from numpy.typing import NDArray
-from typing import Tuple, Union, Optional
+from typing import Tuple, Union, Optional, List, Any, Callable
 from enum import Enum
 
 import numpy as np
@@ -8,6 +10,8 @@ import tkinter as tk
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 
+from .AudioData import AudioData, VersionControlArray
+from .Commands import CommandManager, CommandException
 
 TOOLBAR_POSITION = Enum("TOOLBAR_POSITION", "NONE TOP LEFT RIGHT BOTTOM", start=-1)
 
@@ -18,18 +22,24 @@ class DataViewException(Exception):
 
 class DataView:
     """ Base class creating container for data able to receive callbacks from commands when added as listener """
-    def freq_change_callback(self, index_start : int, index_end : int) -> None:
+    def freq_change_callback(self, data : AudioData, index_start : int, index_end : int) -> None:
         pass
 
 
 class FigureDataView(DataView):
     """ Data Viewer based on pyplot's FigureCanvasTkAgg """
 
-    def figure_clicked(self, event):
+    def _figure_clicked(self, event):
         """ Mouse click callback"""
         pass
 
-    def __init__(self, root : tk.Tk, position : Union[tk.TOP, tk.BOTTOM, tk.LEFT, tk.RIGHT] = tk.TOP, figsize : Tuple[int, int] = (6,5), dpi : int = 100, toolbar_pos : TOOLBAR_POSITION = TOOLBAR_POSITION.NONE):
+    def __init__(self, root: tk.Tk,
+                 position: Union[tk.TOP, tk.BOTTOM, tk.LEFT, tk.RIGHT] = tk.TOP,
+                 figsize: Tuple[int, int] = (6,5),
+                 dpi: int = 100,
+                 toolbar_pos: TOOLBAR_POSITION = TOOLBAR_POSITION.NONE,
+                 command_manager: Optional[CommandManager] = None,
+                 selection_window: Tuple[Union[float, int], Union[float, int]] = (-1, -1)):
         self.root = root
         self.figsize = figsize
         self.dpi = dpi
@@ -37,11 +47,16 @@ class FigureDataView(DataView):
         self.frame = tk.Frame(master=self.root)
         self.figure = plt.Figure(figsize=figsize, dpi=dpi)
         self.ax = self.figure.add_subplot(111)
+        self.command_manager = command_manager
+
+        if selection_window[0] != -1 and selection_window[1] != -1:
+            self.selection_line = self.ax.axvline(x=selection_window[0])
+            self.selection_window = self.ax.axvspan(selection_window[0], selection_window[1] - selection_window[0], alpha=0.5)
 
         # add plots
         self.canvas = FigureCanvasTkAgg(self.figure, master=self.frame)
         self.canvas.draw()
-        self.canvas.mpl_connect("button_press_event", self.figure_clicked)
+        self.canvas.mpl_connect("button_press_event", self._figure_clicked)
 
         # add toolbar and pack everything to the frame
         self.toolbar = NavigationToolbar2Tk(self.canvas, self.frame, pack_toolbar=False)
@@ -75,22 +90,49 @@ class FigureDataView(DataView):
         else:
             raise DataViewException(f"Unexpected figure position: {position}")
 
+    def set_selection_window(self, selection_window : Tuple[Union[float, int], Union[float, int]]):
+        """ update the selection windows"""
+
+        self.selection_line.set_data([selection_window[0], selection_window[0]], [0,1])
+        window_points = self.selection_window.get_xy()
+        window_points[:, 0] = [selection_window[0], selection_window[0],
+                               selection_window[1], selection_window[1], selection_window[0]]
+        self.selection_window.set_xy(window_points)
+
+        self.canvas.draw()
+
 
 class LineFigureDataView(FigureDataView):
     """ Line Plot """
 
-    def __init__(self, root, x, y,
-                 label : Optional[str] = None,
-                 color : Optional[Union[str, Tuple[float, float, float], Tuple[float, float, float, float]]] = None,
-                 alpha : float = 1.0, *args, **kwargs):
+    def __init__(self, root,
+                 data: AudioData,
+                 x: List[Callable[[AudioData], Union[NDArray[Any], VersionControlArray]]],
+                 y: List[Callable[[AudioData], Union[NDArray[Any], VersionControlArray]]],
+                 label: Optional[str] = None,
+                 color: Optional[Union[str, Tuple[float, float, float], Tuple[float, float, float, float]]] = None,
+                 alpha: float = 1.0, *args, **kwargs):
         """ Initialize line plot """
         super().__init__(root, *args, **kwargs)
-        self.line, = self.ax.plot(x, y, label=label, color=color, alpha=alpha)
+        self._data = data
+        self._x = x
+        self._y = y
 
-    def set_data(self, x, y):
+        xx = [x_tmp(self._data) for x_tmp in x]
+        yy = [y_tmp(self._data) for y_tmp in y]
+
+        for i, (x_tmp, y_tmp) in enumerate(zip(xx,yy)):
+            setattr(self, f"line_{i}", self.ax.plot(x_tmp, y_tmp, label=label, color=color, alpha=alpha)[0])
+
+    def set_data(self):
         """ change data """
-        self.line.set_xdata(x)
-        self.line.set_ydata(y)
+
+        xx = [x(self._data) for x in self._x]
+        yy = [y(self._data) for y in self._y]
+
+        for i, (x_tmp, y_tmp) in enumerate(zip(xx,yy)):
+            getattr(self, f"line_{i}").set_xdata(x_tmp)
+            getattr(self, f"line_{i}").set_ydata(y_tmp)
 
         self.canvas.draw()
 
@@ -98,32 +140,79 @@ class LineFigureDataView(FigureDataView):
 class ScatterFigureDataView(FigureDataView):
     """ Scatter plot """
 
-    def __init__(self, root, x, y,
-                 label : Optional[str] = None,
-                 color : Optional[Union[str, Tuple[float, float, float], Tuple[float, float, float, float]]] = None,
-                 alpha : float = 1.0, *args, **kwargs):
+    def __init__(self, root,
+                 data: AudioData,
+                 x: Callable[[AudioData], Union[VersionControlArray, NDArray[Any]]],
+                 y: Callable[[AudioData], Union[VersionControlArray, NDArray[Any]]],
+                 label: Optional[str] = None,
+                 color: Optional[Union[str, Tuple[float, float, float], Tuple[float, float, float, float]]] = None,
+                 alpha: float = 1.0, *args, **kwargs):
         """ Initialize scatter plot """
         super().__init__(root, *args, **kwargs)
-        self.points = self.ax.scatter(x, y, label=label, color=color, alpha=alpha)
+        self._data = data
+        self._x = x
+        self._y = y
+        self.points = self.ax.scatter(x(data), y(data), label=label, color=color, alpha=alpha)
 
-    def set_data(self, x, y):
+    def set_data(self):
         """ change data """
-        data = np.vstack((x, y)).transpose()
+        data = np.vstack((self._x(self._data), self._y(self._data))).transpose()
         self.points.set_offsets(data)
 
 
 class BarFigureDataView(FigureDataView):
     """ Bar plot """
 
-    def __init__(self, root, x, y,
-                 label : Optional[str] = None,
-                 color : Optional[Union[str, Tuple[float, float, float], Tuple[float, float, float, float]]] = None,
-                 alpha : float = 1.0, *args, **kwargs):
+    def __init__(self, root,
+                 data : AudioData,
+                 x: Callable[[AudioData], Union[VersionControlArray, NDArray[Any]]],
+                 y: Callable[[AudioData], Union[VersionControlArray, NDArray[Any]]],
+                 label: Optional[str] = None,
+                 color: Optional[Union[str, Tuple[float, float, float], Tuple[float, float, float, float]]] = None,
+                 alpha: float = 1.0, *args, **kwargs):
         """ Initialize bar plot """
         super().__init__(root, *args, **kwargs)
-        self.bars = self.ax.bar(x, y, label=label, color=color, alpha=alpha)
+        self._data = data
+        self._x = x
+        self._y = y
+        self.bars = self.ax.bar(x(data), y(data), label=label, color=color, alpha=alpha)
 
-    def set_data(self, y):
+    def set_data(self):
         """ change data """
+        y = self._y(self._data)
         for j, b in enumerate(self.bars):
             b.set_height(y[j])
+
+
+def x_data_freq(data: AudioData) -> NDArray[np.float32]:
+    """ return frequency x grid"""
+    return data.freq_x
+
+
+def y_data_freq_abs(data: AudioData) -> NDArray[np.float32]:
+    """ return absolute value of normalized frequency"""
+    return np.abs(data.norm_freq())
+
+
+def y_data_freq_real(data: AudioData) -> NDArray[np.float32]:
+    """ returns real part of normalized frequencies """
+    return np.real(data.norm_freq())
+
+
+def y_data_freq_imag(data: AudioData) -> NDArray[np.float32]:
+    """ returns imaginary part of normalized frequencies """
+    return np.imag(data.norm_freq())
+
+
+class EqualizerFigureDataView(LineFigureDataView):
+    """ Equalizer plot """
+
+    def freq_change_callback(self, data: AudioData, index_start: int, index_end: int) -> None:
+        self.set_data()
+
+    def _figure_clicked(self, event):
+        """ Mouse click callback"""
+        if self.command_manager is None:
+            raise CommandException("DataView does not know about the CommandManager. Callback can not be started")
+        self.command_manager.call("equ_clicked", event)
+        self.set_data()
