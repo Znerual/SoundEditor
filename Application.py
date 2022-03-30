d6ed8eeefe23f051f4f644d96f6ab701028a970e
Laurenz Ruzicka <laurenz_ruzicka@gmx.at>
2022-03-30 22:30:09 +0200

    Changed callback structure and added timeline view

--- Application.py
+++ Application.py
@@ -2,9 +2,9 @@ import tkinter as tk
 import numpy as np
 
 from SoundEditor.AudioData import AudioData, VersionControlArray
-from SoundEditor.Commands import CommandManager, SetFreq, equalizer_callback
-from SoundEditor.DataView import LineFigureDataView,  BarFigureDataView,  EqualizerFigureDataView, TOOLBAR_POSITION
-from SoundEditor.DataView import x_data_freq, y_data_freq_imag, y_data_freq_real, y_data_freq_abs
+from SoundEditor.Commands import CommandManager, SetFreq, equalizer_callback, timeline_callback
+from SoundEditor.DataView import TimeLineFigureDataView,  BarFigureDataView,  EqualizerFigureDataView, TOOLBAR_POSITION
+from SoundEditor.DataView import x_data_freq, y_data_freq_imag, y_data_freq_real, y_data_freq_abs, x_data_time, y_data_time
 
 
 
@@ -21,7 +21,7 @@ def main():
 
     audio_data = AudioData.from_file("test.wav")
     manager = CommandManager(audio_data)
-    manager.register_callback("equ_clicked", equalizer_callback)
+
     start_index = 80000
     end_index = 81024
     audio_data.ft(start_index, end_index)
@@ -37,6 +37,19 @@ def main():
                                      position=tk.LEFT,
                                      toolbar_pos=TOOLBAR_POSITION.TOP)
 
+    manager.register_callback("equ_clicked", equalizer_callback, [equ_dv])
+
+    time_dv = TimeLineFigureDataView(root=time_frame,
+                                 data=audio_data,
+                                 x=[x_data_time],
+                                 y=[y_data_time],
+                                 selection_window=(start_index / audio_data.fs, end_index / audio_data.fs),
+                                 command_manager=manager,
+                                 position=tk.TOP,
+                                 dpi=100,
+                                 figsize=(6,2))
+
+    manager.register_callback("timeline_clicked", timeline_callback, [equ_dv, time_dv])
 
     #bar_dv = BarFigureDataView(frequency_frame,
     #                           x=np.arange(1,7,1, dtype=int),
