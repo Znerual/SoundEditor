941eba3584b869ef7add3df8a019bf9600d8ddb6
Laurenz Ruzicka <laurenz_ruzicka@gmx.at>
2022-04-13 22:37:32 +0200

    Read available ffmpeg formats Use pydub to read and write in all audio formats Do initial ft in from_file init

--- src/SoundEditor/AudioData.py
+++ src/SoundEditor/AudioData.py
@@ -1,8 +1,13 @@
 from __future__ import annotations
 
+import subprocess
+import pathlib
+
 import numpy as np
 
-from typing import Optional
+from typing import Optional, Tuple, Union, Dict, List
+
+import pydub
 from numpy.typing import NDArray
 from dataclasses import dataclass, field
 
@@ -90,6 +95,53 @@ class VersionControlArray:
         return self._history
 
 
+def ffmpeg_formats() -> List[Tuple[str, str]]:
+    """
+    finds available formats
+    """
+
+    formats = []
+    process = subprocess.Popen(['ffmpeg', '-formats'], shell=True, stdout=subprocess.PIPE)
+
+    # skip header information
+    for _ in range(4):
+        process.stdout.readline()
+
+    while line := process.stdout.readline().decode('ascii'):
+        parts = line[4:].rstrip("\r\n").split(" ")
+        formats.append((" ".join(filter(lambda x: x != "",parts[1:])), parts[0]))
+
+    return sorted(formats)
+
+
+def pydub_to_np(audio: pydub.AudioSegment) -> (NDArray[np.float32], int):
+    """
+    Converts pydub audio segment into np.float32 of shape [duration_in_seconds*sample_rate, channels],
+    where each value is in range [-1.0, 1.0].
+    Returns tuple (audio_np_array, sample_rate).
+    """
+    return np.array(audio.get_array_of_samples(), dtype=np.float32).reshape((-1, audio.channels)) / (
+            1 << (8 * audio.sample_width - 1)), audio.frame_rate, audio.sample_width, audio.channels
+
+
+def np_to_pydub(array: NDArray[np.float32], fs: int, sample_width: int = 4, channels: int = 2) -> pydub.AudioSegment:
+    """
+    Converts a given numpy array into a pydub AudioSegment
+    """
+    if sample_width == 4:
+        int_array = np.int32(array.reshape(-1, ) * 2 ** 31)
+    elif sample_width == 2:
+        int_array = np.int16(array.reshape(-1, ) * 2 ** 15)
+    elif sample_width == 8:
+        int_array = np.int64(array.reshape(-1, ) * 2 ** 63)
+    else:
+        raise AudioDataException(f"Invalid sample width {sample_width} for the conversion to a numpy array")
+
+    audio = pydub.AudioSegment(int_array.tobytes(), frame_rate=fs, sample_width=sample_width, channels=channels)
+
+    return audio
+
+
 @dataclass
 class AudioData:
     _time_data: VersionControlArray = field(init=False)
@@ -101,23 +153,41 @@ class AudioData:
     _freq_sel_start_ind: int = -1
     _freq_sel_end_ind: int = -1
     _a_coef: float = 0.25
+    _channels: int = -1
+    _sample_width: int = -1
 
     @classmethod
     def from_file(cls, filename: str) -> AudioData:
         """ Initializer from a file"""
-        import soundfile as sf
+        # import soundfile as sf
 
         # create new instance and call parent initializer
         instance: AudioData = cls.__new__(cls)
         super(cls, instance).__init__()
 
         # fill audio data with loaded information
-        raw_time_data, instance._fs = sf.read(filename, dtype=np.double)
+        sound = pydub.AudioSegment.from_file(filename)
+        raw_time_data, instance._fs, instance._sample_width, instance._channels = pydub_to_np(
+            sound)  # sf.read(filename, dtype=np.double)
         instance._time_data = VersionControlArray(raw_time_data)
         instance._seconds = instance._time_data.shape[0] / instance.fs
         instance._time_x = np.arange(0, instance.seconds, 1 / instance.fs, dtype=np.float32)
 
+        # do the initial fourier transform
+        if instance.seconds > 10:
+            instance.ft(0, 10 * instance.fs)
+        else:
+            instance.ft(0, instance.time.shape[0])
+
         return instance
+
+    def save_to_file(self, filename: Union[pathlib.Path, str], file_format: str = "wav") -> None:
+        """
+        saves the curent audio data to a file
+        All file endings supported by pydub (== all endings supported by ffmpeg)
+        """
+        audio = np_to_pydub(self.time.view(), fs=self.fs, sample_width=self._sample_width, channels=self._channels)
+        audio.export(filename, format=file_format)
 
     def _four_trans_seq(self) -> None:
         # TODO: convolute with decaying funciton to avoid boundary effects
@@ -136,9 +206,11 @@ class AudioData:
         # create version controlled array and fill without creating history
         self._freq_data = VersionControlArray.empty((N, 2), dtype="complex64")
         self._freq_data.set_no_undo((slice(None, None, 1), 0),
-                                    fft(damp_func * self._time_data[self._freq_sel_start_ind:self._freq_sel_end_ind, 0]))
+                                    fft(damp_func * self._time_data[self._freq_sel_start_ind:self._freq_sel_end_ind,
+                                                    0]))
         self._freq_data.set_no_undo((slice(None, None, 1), 1),
-                                    fft(damp_func * self._time_data[self._freq_sel_start_ind:self._freq_sel_end_ind, 1]))
+                                    fft(damp_func * self._time_data[self._freq_sel_start_ind:self._freq_sel_end_ind,
+                                                    1]))
 
     def find_peaks(self, n_dom: int = 6, chanel: int = 0) -> NDArray[np.int32]:
         """ find peaks in the frequency spectrum """
@@ -181,15 +253,17 @@ class AudioData:
 
     @property
     def damping(self) -> NDArray[np.float32]:
+        """ returns the damping function which is used on the time data before Fourier transformation"""
         a = self._a_coef
         x = np.linspace(0, 1, num=self.N)
-        return np.exp((-(x - 0.5) ** 2) / (2*a ** 2)) #self._time_x[0:self.N] - self._time_x[self.N//2]
+        return np.exp((-(x - 0.5) ** 2) / (2 * a ** 2))  # self._time_x[0:self.N] - self._time_x[self.N//2]
 
     @property
     def inv_damping(self) -> NDArray[np.float32]:
+        """ returns the inverse damping function which is used on the time data after the inverse FT"""
         a = self._a_coef
         x = np.linspace(0, 1, num=self.N)
-        return np.exp(((x - 0.5) ** 2) / (2*a ** 2))
+        return np.exp(((x - 0.5) ** 2) / (2 * a ** 2))
 
     def ft(self, start_index: int, end_index: Optional[int] = None):
         """ Fourier transforms the given audio segment """
@@ -203,18 +277,18 @@ class AudioData:
         from scipy.fft import ifft
 
         inv_damp_func = self.inv_damping
-        self._time_data[self.start_index:self.end_index, 0] = np.real(inv_damp_func * ifft(self.freq[:,0]))
-        self._time_data[self.start_index:self.end_index, 1] = np.real(inv_damp_func * ifft(self.freq[:,1]))
+        self._time_data[self.start_index:self.end_index, 0] = np.real(inv_damp_func * ifft(self.freq[:, 0]))
+        self._time_data[self.start_index:self.end_index, 1] = np.real(inv_damp_func * ifft(self.freq[:, 1]))
 
     @property
     def freq(self) -> VersionControlArray:
         """ Get view of frequency data Version Control Array and generate Fourier Transform if not existing """
         return self._freq_data
 
-    def norm_freq(self,  chanel: int = 0) -> NDArray[np.csingle]:
+    def norm_freq(self, chanel: int = 0) -> NDArray[np.csingle]:
         """ Returns a copy of the normalized frequencies"""
 
-        return self.freq[:self._freq_data.shape[0]//2, chanel] * 2.0 / self.N
+        return self.freq[:self._freq_data.shape[0] // 2, chanel] * 2.0 / self.N
 
     @property
     def freq_x(self) -> NDArray[np.float32]:
